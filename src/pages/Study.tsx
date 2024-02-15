e62afe2d8b1f5de2eb9ccb8c84ed852eacda6c3f
Kyle <kyle.chutjian@gmail.com>
2024-02-14 20:06:32 -0500

    added skip button, added flashcardscore type

--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -5,10 +5,11 @@ import { getFlashcardsByCollectionIds } from "../api";
 const lodash = require("lodash");
 
 /* 
-1. Setup frontend - DONE
-2. Get list of flashcards in selectedCollections - DONE
-3. Randomize the flashcards
-4. Show current flashcard's collection name
+1. Show current flashcard's collection name
+2. Display front/back flashcard language
+3. Current score/total score
+4. Skip button
+5. Navigate to results screen when done
 */
 
 type Flashcard = {
@@ -20,14 +21,26 @@ type Flashcard = {
   createdAt: string;
 }
 
+type FlashcardScore = {
+  flashcard_id: string;
+  collection_id: string;
+  front: string;
+  isCorrect: boolean;
+}
+
 const Study = () => {
     const selectedCollections = useAppSelector(state => state.collections.collections);
     const frontFlashcardLanguage = useAppSelector(state => state.collections.frontFlashcard);
     const backFlashcardLanguage = useAppSelector(state => state.collections.backFlashcard);
     
-    const [ frontFlashcards, setFrontFlashcards ] = useState<Array<string> | null>(null);
+    const [ frontFlashcards, setFrontFlashcards ] = useState<Array<FlashcardScore> | null>(null);
     const [ backFlashcards, setBackFlashcards ] = useState<Array<string> | null>(null);
-    const [ currentFrontFlashcard, setCurrentFrontFlashcard ] = useState("");
+    const [ currentFrontFlashcard, setCurrentFrontFlashcard ] = useState<FlashcardScore>({
+      flashcard_id: "",
+      collection_id: "",
+      front: "",
+      isCorrect: false
+    });
     const [ currentBackFlashcard, setCurrentBackFlashcard ] = useState("");
     const [ currentFlashcardIndex, setCurrentFlashcardIndex ] = useState(0);
     const [ totalFlashcardIndex, setTotalFlashcardIndex ] = useState(0);
@@ -39,9 +52,24 @@ const Study = () => {
       getFlashcardsByCollectionIds({collectionIds: selectedCollections}).then((res) => {
         const shuffledFlashcards = lodash.shuffle(res.data);
         const shuffledFrontFlashcards = shuffledFlashcards.map((flashcard: Flashcard) => {
-          if (frontFlashcardLanguage === "english") return flashcard.english;
-          if (frontFlashcardLanguage === "romaji") return flashcard.romaji;
-          if (frontFlashcardLanguage === "kana") return flashcard.kana;
+          if (frontFlashcardLanguage === "english") return {
+            flashcard_id: flashcard.flashcard_id, 
+            collection_id: flashcard.collection_id,
+            front: flashcard.english, 
+            isCorrect: false
+          };
+          if (frontFlashcardLanguage === "romaji") return {
+            flashcard_id: flashcard.flashcard_id, 
+            collection_id: flashcard.collection_id,
+            front: flashcard.romaji, 
+            isCorrect: false
+          };
+          if (frontFlashcardLanguage === "kana") return {
+            flashcard_id: flashcard.flashcard_id, 
+            collection_id: flashcard.collection_id,
+            front: flashcard.kana, 
+            isCorrect: false
+          };
           else return null;
         });
 
@@ -62,33 +90,45 @@ const Study = () => {
 
     const onChangeInput = (newInput: string) => {
       setInputText(newInput);
+      if (frontFlashcards) console.log(frontFlashcards[currentFlashcardIndex])
 
       if (newInput.toLowerCase() === currentBackFlashcard.toLowerCase()) {
-        console.log(`${currentFlashcardIndex} : ${totalFlashcardIndex}`)
+        setFrontFlashcards((currentFrontFlashcards: Array<FlashcardScore> | null) => {
+          if (!currentFrontFlashcards) return null;
+          const updatedFrontFlashcards = [...currentFrontFlashcards];
+          updatedFrontFlashcards[currentFlashcardIndex].isCorrect = true;
+          return updatedFrontFlashcards;
+        });
+        console.log(frontFlashcards);
+
         if (currentFlashcardIndex+1 === totalFlashcardIndex) {
           console.log("DONE!");
           setInputText("");
-          setCurrentFrontFlashcard("DONE");
         } else {
           if (frontFlashcards && backFlashcards) {
-            console.log("changing to new flashcard:");
             setCurrentFrontFlashcard(frontFlashcards[currentFlashcardIndex+1])
             setCurrentBackFlashcard(backFlashcards[currentFlashcardIndex+1])
             setCurrentFlashcardIndex(currentFlashcardIndex+1);
             setInputText("");
-          } else {
-            console.log(frontFlashcards);
-            console.log(backFlashcards);
           }
-
-
-
         }
 
 
         console.log("CORRECT");
       }
-
+    }
+    const onSkip = () => {
+      if (currentFlashcardIndex+1 === totalFlashcardIndex) {
+        console.log("DONE!");
+        setInputText("");
+      } else {
+        if (frontFlashcards && backFlashcards) {
+          setCurrentFrontFlashcard(frontFlashcards[currentFlashcardIndex+1])
+          setCurrentBackFlashcard(backFlashcards[currentFlashcardIndex+1])
+          setCurrentFlashcardIndex(currentFlashcardIndex+1);
+          setInputText("");
+        }
+      }
 
     }
 
@@ -100,15 +140,17 @@ const Study = () => {
         <div className="max-w-md mx-auto mt-5 flex flex-col items-center mt-[10rem]">
             <div className="mb-5 min-w-[70%] max-w-[100%]">
               <h1 className="text-4xl font-bold tracking-tight py-5 text-center border max-w-[100%] mb-10 h-auto overflow-hidden">
-                <span className="inline-block max-w-full break-all overflow-ellipsis">{currentFrontFlashcard}</span>
+                <span className="inline-block max-w-full break-all overflow-ellipsis">{currentFrontFlashcard.front}</span>
               </h1>
-                <input 
-                    type="text" 
-                    name="input"
-                    value={inputText}
-                    onChange={(e) => onChangeInput(e.target.value)}
-                    className="shadow-sm border-b-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
-                />
+              <input 
+                  type="text" 
+                  name="input"
+                  value={inputText}
+                  onChange={(e) => onChangeInput(e.target.value)}
+                  className="shadow-sm border-b-2 border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 shadow-sm-light" 
+              />
+              <button onClick={() => onSkip()} className="mt-5 flex justify-center mx-auto w-[30%] t-5 text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Skip</button> 
+              
             </div> 
         </div>
     </section>
